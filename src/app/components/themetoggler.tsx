c785d6a047d9c4318395367d4280c12b6c8bcf7b
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2025-08-29 12:48:13 +0530

    Readme changes

---
+++ src/app/components/themetoggler.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Sun, Moon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return <div className="h-10 w-10" />
+
+    return (
+        <Button
+            className="min-w-10 min-h-10"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+            {theme === "light" ? <Moon /> : <Sun />}
+        </Button>
+    )
+}
+
+export default ThemeToggle
