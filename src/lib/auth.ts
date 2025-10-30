1b7b6da78933d32507f006a1d7cfa50b355a27f6
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2025-10-30 13:44:28 +0530

    Google authentication added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,6 +19,13 @@ export const auth = betterAuth({
             });
         },
     },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            prompt: "select_account"
+        },
+    },
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: schema,
