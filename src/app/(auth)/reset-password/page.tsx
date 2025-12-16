5a39d1518bc023973dfebc290e95c938d8b1f21f
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2025-12-16 15:33:59 +0530

    some minor improvements in auth routes

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -3,6 +3,7 @@ import { ResetPassForm } from "@/components/resetpass-form";
 import { Suspense } from "react";
 import { Spinner } from "@/components/ui/spinner";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Reset Password - Testimonia",
@@ -14,12 +15,12 @@ export default function Page() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium">
+        <Link href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
           Testimonial
-        </a>
+        </Link>
         <Suspense
           fallback={
             <div className="flex items-center justify-center h-screen">
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react";
 import { SignupForm } from "@/components/signup-form";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Signup - Testimonia",
@@ -8,17 +9,19 @@ export const metadata: Metadata = {
     "Testimonia helps creators and businesses collect testimonials, manage social proof, and embed beautiful widgets on any website. Simple, fast, and privacy-friendly.",
 };
 
-
 export default function Page() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
-          Testimonial
-        </a>
+          Testimonia
+        </Link>
         <SignupForm />
       </div>
     </div>
